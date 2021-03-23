7b4b907d171f6a49bd57338a89f12ff8459bdb91
Amanda-Katherine <walker.akw@gmail.com>
2021-03-22 20:32:09 -0400

    delete default code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 import Particles from 'react-particles-js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-// function App() {
 class App extends React.Component {
   render() {
     return (
