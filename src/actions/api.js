d23886fac068322af77c8745ce10e71a09cbd757
Amanda-Katherine <walker.akw@gmail.com>
2021-03-22 20:57:10 -0400

    create fetchWorldData api request

--- src/actions/api.js
+++ src/actions/api.js
@@ -2,3 +2,16 @@ const worldTotalUrl = 'https://disease.sh/v3/covid-19/all';
 const worldHistoryUrl =
   'https://disease.sh/v3/covid-19/historical/all?lastdays=all';
 
+export const fetchWorldData = async () => {
+  const response = await fetch(`${worldTotalUrl}`);
+  const data = await response.json();
+  let worldVariables = {
+    totalCases: data.cases,
+    todayCases: data.todayCases,
+    totalDeaths: data.deaths,
+    todayDeaths: data.todayDeaths,
+  };
+  console.log(data);
+  console.log(worldVariables);
+  return worldVariables;
+};
