0c35f47fa0caa1ea09db0a6a8f52e9de8d495113
Amanda-Katherine <walker.akw@gmail.com>
2021-03-22 21:09:41 -0400

    declare variable for props data and create function to map historical data points to proper format

--- src/components/dataConverter.js
+++ src/components/dataConverter.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 class DataConverter extends Component {
+  mapDataPoints = () => {
+  };
   render() {
+    let worldData = this.props.worldHistoryData;
+
     return (
       <div>
+        <>{worldData && this.mapDataPoints()}</>
       </div>
     );
   }
