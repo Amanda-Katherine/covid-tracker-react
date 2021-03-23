469e0de7da6bcf92bf91ebd52b36d1cfae466fd1
Amanda-Katherine <walker.akw@gmail.com>
2021-03-22 21:28:05 -0400

    import and call ant-design globe icon

--- src/components/GridCards/tallyCard.js
+++ src/components/GridCards/tallyCard.js
@@ -1,5 +1,7 @@
 import { Card, Col, Row } from 'antd';
 import React from 'react';
+import { GlobalOutlined } from '@ant-design/icons';
+
 export default function tallyCard(data) {
   let { totalCases, todayCases, totalDeaths, todayDeaths } = data.worldData;
   //add in conditional for worldData or countryData once added in.
@@ -9,6 +11,7 @@ export default function tallyCard(data) {
     <div className="site-card-wrapper">
       <Row gutter={16}>
         <Col span={8}>
+          <GlobalOutlined />
           <Card title={title} bordered={false}>
             <hr />
           </Card>
