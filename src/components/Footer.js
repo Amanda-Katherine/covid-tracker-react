f53baa72d02d9d4950bab7ee41823f86602aaab3
Amanda-Katherine <walker.akw@gmail.com>
2021-03-22 20:33:31 -0400

    comment out undesired code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,27 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+// import Link from '@material-ui/core/Link';
+
+// function Copyright() {
+//   return (
+//     <Typography variant="body2" color="textSecondary">
+//       {'Copyright © '}
+//       <Link color="inherit" href="https://material-ui.com/">
+//         Your Website
+//       </Link>{' '}
+//       {new Date().getFullYear()}
+//       {'.'}
+//     </Typography>
+//   );
+// }
+
 const useStyles = makeStyles((theme) => ({
+  //   root: {
+  //     display: 'flex',
+  //     flexDirection: 'column',
+  //     minHeight: '100vh',
+  //   },
   main: {
     marginTop: theme.spacing(2),
     marginBottom: theme.spacing(5),
@@ -29,6 +49,7 @@ export default function StickyFooter() {
         <Container maxWidth="sm">
           <Typography variant="body1" color="textSecondary">
           </Typography>
+          {/* <Copyright /> */}
         </Container>
       </footer>
     </div>
