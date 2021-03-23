1acedc76d5c7860983749abd960328f41c23c26e
Amanda-Katherine <walker.akw@gmail.com>
2021-03-22 21:09:54 -0400

    initialize total cases

--- src/components/dataConverter.js
+++ src/components/dataConverter.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 class DataConverter extends Component {
   mapDataPoints = () => {
+    let totalCases = this.props.worldHistoryData.totalCases || {};
   };
   render() {
     let worldData = this.props.worldHistoryData;
