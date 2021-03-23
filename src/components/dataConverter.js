3f95fd75429117ed6c05f53662102523b7537314
Amanda-Katherine <walker.akw@gmail.com>
2021-03-22 21:12:07 -0400

    set conditional so code isn't called before data has loaded

--- src/components/dataConverter.js
+++ src/components/dataConverter.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 class DataConverter extends Component {
   mapDataPoints = () => {
     let totalCases = this.props.worldHistoryData.totalCases || {};
+    if (!!totalCases !== {}) {
+    }
   };
   render() {
     let worldData = this.props.worldHistoryData;
