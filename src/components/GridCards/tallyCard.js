7525fb97e3abe3ba29cf8cf47f30fc317531f118
Amanda-Katherine <walker.akw@gmail.com>
2021-03-22 21:25:39 -0400

    set dynamic title for card

--- src/components/GridCards/tallyCard.js
+++ src/components/GridCards/tallyCard.js
@@ -3,10 +3,14 @@ import React from 'react';
 export default function tallyCard(data) {
   let { totalCases, todayCases, totalDeaths, todayDeaths } = data.worldData;
   //add in conditional for worldData or countryData once added in.
+  let title = data.worldData ? 'Global Stats' : 'US Stats';
+
   return (
     <div className="site-card-wrapper">
       <Row gutter={16}>
         <Col span={8}>
+          <Card title={title} bordered={false}>
+          </Card>
         </Col>
         <Col span={4}>
         </Col>
