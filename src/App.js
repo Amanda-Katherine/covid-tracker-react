c2ab97904fca31ba7894772ac1350a49ead43b71
Amanda-Katherine <walker.akw@gmail.com>
2021-03-22 20:30:32 -0400

    import Particles Background Code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import React from 'react';
 import './App.css';
+import Particles from 'react-particles-js';
 
 function App() {
   return (
@@ -27,7 +28,62 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Particles
+          className="particles-canvas"
+          params={{
+            particles: {
+              number: {
+                value: 53,
+                density: {
+                  enable: true,
+                  value_area: 561,
+                },
+              },
 
+              opacity: {
+                value: 0.0561194221302933,
+                random: true,
+              },
+              size: {
+                value: 3,
+              },
+              line_linked: {
+                enable: true,
+                distance: 150,
+                color: '#ffffff',
+                opacity: 0.4,
+                width: 1,
+              },
+              move: {
+                speed: 1.6,
+                direction: 'none',
+
+                bounce: false,
+              },
+            },
+            interactivity: {
+              detect_on: 'canvas',
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: 'repulse',
+                },
+                onclick: {
+                  enable: true,
+                  mode: 'push',
+                },
+                resize: true,
+              },
+              modes: {
+                repulse: {
+                  distance: 73.08694910712106,
+                  duration: 0.4,
+                },
+              },
+            },
+            retina_detect: true,
+          }}
+        />
       </div>
     );
   }
